ac6115f307f903e568c69c6b8cb051a174569533
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2024-10-01 20:49:25 -0400

    RockPaperScissors game is ready and working

--- app/src/main/java/com/saunakpatel/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/saunakpatel/rockpaperscissors/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 
 
@@ -27,14 +28,97 @@ class MainActivity : AppCompatActivity() {
     private var roundCnt = 0;
     private var scoreCPU = 0;
     private var scoreHuman = 0;
+    private var cpuChoice = 0;
+    private var humanChoice = 0;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         cpuHand = findViewById<ImageView>(R.id.cpuHand)
+        humanHand = findViewById<ImageView>(R.id.humanHand)
 
+        roundNum = findViewById<TextView>(R.id.roundNum)
+        cpuScore = findViewById<TextView>(R.id.cpuScore)
+        humanScore = findViewById<TextView>(R.id.humanScore)
+        reset = findViewById<Button>(R.id.reset)
 
+        choiceRock = findViewById<ImageButton>(R.id.choiceRock)
+        choicePaper = findViewById<ImageButton>(R.id.choicePaper)
+        choiceScissors = findViewById<ImageButton>(R.id.choiceScissors)
 
+        humanHand.setImageResource(R.drawable.rps_blank)
+        cpuHand.setImageResource(R.drawable.rps_blank)
+
+        choiceRock.setOnClickListener{_ ->
+            humanChoice = 0;
+            play(humanChoice)
         }
+
+        choicePaper.setOnClickListener{_ ->
+            humanChoice = 1;
+            play(humanChoice)
+        }
+
+        choiceScissors.setOnClickListener{_ ->
+            humanChoice = 2;
+            play(humanChoice)
+        }
+
+        reset.setOnClickListener{_ ->
+            reset()
+        }
+
     }
+
+    private fun play(choice: Int){
+
+        when (choice) {
+            0 -> humanHand.setImageResource(R.drawable.rps_rock)
+            1 -> humanHand.setImageResource(R.drawable.rps_paper)
+            else -> humanHand.setImageResource(R.drawable.rps_scissors)
+        }
+
+        val random = Random.nextInt(3)
+
+        when (random) {
+            0 -> cpuHand.setImageResource(R.drawable.rps_rock)
+            1 -> cpuHand.setImageResource(R.drawable.rps_paper)
+            else -> cpuHand.setImageResource(R.drawable.rps_scissors)
+        }
+
+        helper(choice, random)
+
+        if (roundCnt == 20) reset()
+    }
+
+    private fun reset(){
+        roundNum.text = "0"
+        cpuScore.text = "0"
+        humanScore.text = "0"
+
+        humanHand.setImageResource(R.drawable.rps_blank)
+        cpuHand.setImageResource(R.drawable.rps_blank)
+
+        roundCnt = 0
+        scoreCPU = 0
+        scoreHuman = 0
+
+    }
+
+    private fun helper(choice: Int, random: Int){
+        if(choice == random) roundCnt += 1
+        else if(choice == 1 && random == 0 || choice == 0 && random == 2 || choice == 2 && random == 1){
+            roundCnt += 1
+            scoreHuman += 1
+        } else {
+            roundCnt += 1
+            scoreCPU += 1
+        }
+        cpuScore.text = "" + scoreCPU
+        humanScore.text = "" + scoreHuman
+        roundNum.text = "" + roundCnt
+    }
+
+}
